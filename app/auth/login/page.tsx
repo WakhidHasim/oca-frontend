89e731474b913ab69ab9e577eb7859fee823087c
imlana21 <ihwan.cfc@gmail.com>
2023-11-30 23:34:37 +0700

    verifikasi

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,11 +1,10 @@
 import Image from "next/image";
 
-
 export default function Login() {
   return (
-    <div className="flex items-center justify-center h-screen" id="login">
-      <div className="bg-[#700070] flex w-1/2">
-        <div className="text-white px-8 py-10">
+    <div className="flex items-center justify-center h-screen">
+      <div className="bg-[#ba90bc] flex w-1/2 rounded-md">
+        <div className="bg-[#700070] text-white px-8 py-10 rounded-md">
           <div className="">
             <Image src="/no-image.png" alt="No Image" width={195} height={160} />
           </div>
@@ -14,7 +13,7 @@ export default function Login() {
             <div className="mt-2"> LOGIN TO CONTINUE </div>
           </div>
         </div>
-        <div className="bg-gray-50 bg-opacity-60 px-8 py-10 w-2/3">
+        <div className="px-8 py-10 w-2/3">
           <form className="flex flex-col">
             <div className="form-group">
               <label htmlFor="in-nik"> NIK </label>
